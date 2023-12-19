42d0ddec5205c1912b2bcebf5d4a0ecdee6f5dd0
00lar <eduardani84@gmail.com>
2023-12-19 16:19:27 -0700

    new register project design

--- lib/src/forms/alta_form_proyectos.dart
+++ lib/src/forms/alta_form_proyectos.dart
@@ -26,110 +26,107 @@ class _AltaProyectoPageState extends State<AltaProyectoPage> {
       ),
       body: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.only(top: 15, left: 15),
-            width: size.width > 600 ? size.width * 0.8 : 500,
-            child: const Text(
-              'Complete cada uno de los campos para dar de alta un nuevo proyecto',
-              style: TextStyle(
-                fontSize: 18,
-                color: Color.fromARGB(255, 45, 45, 45),
-              ),
-            ),
-          ),
           formview(context),
         ],
       ),
     );
   }
 
-  Container formview(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 70, left: 15, right: 20),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _nombreController,
-              decoration: const InputDecoration(
-                labelText: 'Nombre del Proyecto',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
+  Center formview(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 600), //este es el max width
+        padding: const EdgeInsets.only(top: 70, left: 40, right: 40),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Ingresa todos los datos del nuevo proyecto",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa el nombre del proyecto';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            _buildDateTimePicker(
-              labelText: 'Fecha de inicio',
-              selectedDate: _inicioDate,
-              onDateSelected: (date) {
-                setState(() {
-                  _inicioDate = date;
-                });
-              },
-            ),
-            const SizedBox(height: 16.0),
-            _buildDateTimePicker(
-              labelText: 'Fecha de finalización',
-              selectedDate: _finDate,
-              onDateSelected: (date) {
-                setState(() {
-                  _finDate = date;
-                });
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _costoController,
-              decoration: const InputDecoration(
-                labelText: 'Costo',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa el costo total';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              width: double.infinity,
-              child: FilledButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    String nombre = _nombreController.text;
-                    String fechaInicio = _inicioDate.toString();
-                    String fechaFinalizada = _finDate.toString();
-                    String costo = _costoController.text;
-
-                    postProyecto(nombre, fechaInicio, fechaFinalizada, costo);
-
-                    print('Proyecto dado de alta: $nombre');
-                    Navigator.pop(context);
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SuccessfulScreen(),
-                      ),
-                    );
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _nombreController,
+                decoration: const InputDecoration(
+                  labelText: 'Nombre del Proyecto',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa el nombre del proyecto';
                   }
+                  return null;
                 },
-                child: const Text('Guardar'),
               ),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              _buildDateTimePicker(
+                labelText: 'Fecha de inicio',
+                selectedDate: _inicioDate,
+                onDateSelected: (date) {
+                  setState(() {
+                    _inicioDate = date;
+                  });
+                },
+              ),
+              const SizedBox(height: 16.0),
+              _buildDateTimePicker(
+                labelText: 'Fecha de finalización',
+                selectedDate: _finDate,
+                onDateSelected: (date) {
+                  setState(() {
+                    _finDate = date;
+                  });
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _costoController,
+                decoration: const InputDecoration(
+                  labelText: 'Costo',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa el costo total';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      String nombre = _nombreController.text;
+                      String fechaInicio = _inicioDate.toString();
+                      String fechaFinalizada = _finDate.toString();
+                      String costo = _costoController.text;
+
+                      postProyecto(nombre, fechaInicio, fechaFinalizada, costo);
+
+                      print('Proyecto dado de alta: $nombre');
+                      Navigator.pop(context);
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SuccessfulScreen(),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Guardar'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
