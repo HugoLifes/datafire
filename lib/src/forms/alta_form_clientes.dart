07cc219699cf3ad8533791bb0edb5f45ffceb64a
00lar <eduardani84@gmail.com>
2023-12-20 11:29:02 -0600

    register restructured

--- lib/src/forms/alta_form_clientes.dart
+++ lib/src/forms/alta_form_clientes.dart
@@ -19,114 +19,106 @@ class _AltaClientePageState extends State<AltaClientePage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Agregar nuevo cliente"),
+      ),
       body: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.only(top: 5, left: 15),
-            width: size.width > 600 ? size.width * 0.8 : 500,
-            child: const Text(
-              'Agregar nuevo cliente',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 45, left: 15),
-            width: size.width > 600 ? size.width * 0.8 : 500,
-            child: const Text(
-              'Complete cada uno de los campos para dar de alta un nuevo cliente',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-          ),
           formview(context),
         ],
       ),
     );
   }
 
-  Container formview(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 110, left: 15, right: 20),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _nombreController,
-              decoration: const InputDecoration(
-                labelText: 'Nombre del Cliente',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
+  Center formview(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 600),
+        padding: const EdgeInsets.only(top: 110, left: 15, right: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                           const Text(
+                "Ingresa todos los datos del nuevo cliente",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa el nombre del cliente';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _apellidoController,
-              decoration: const InputDecoration(
-                labelText: 'Apellidos',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa los apellidos del cliente';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _companyController,
-              decoration: const InputDecoration(
-                labelText: 'Compañia',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa la compañía a la que pertenece el cliente';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              width: double.infinity, // Ocupar todo el ancho disponible
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    String nombreCliente = _nombreController.text;
-                    String apellidoCliente = _apellidoController.text;
-                    String companyCliente = _companyController.text;
-                    // Lógica para dar de alta el cliente
-                    postCliente(nombreCliente, apellidoCliente, companyCliente);
-                    // Puedes llamar a una función o realizar cualquier otra acción aquí
-                    print('Cliente dado de alta: $nombreCliente');
-                    Navigator.pop(context);
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SuccessfulScreen()),
-                    );
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _nombreController,
+                decoration: const InputDecoration(
+                  labelText: 'Nombre del Cliente',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa el nombre del cliente';
                   }
+                  return null;
                 },
-                child: const Text('Guardar'),
               ),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _apellidoController,
+                decoration: const InputDecoration(
+                  labelText: 'Apellidos',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa los apellidos del cliente';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _companyController,
+                decoration: const InputDecoration(
+                  labelText: 'Compañia',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa la compañía a la que pertenece el cliente';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                width: double.infinity, // Ocupar todo el ancho disponible
+                child: FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      String nombreCliente = _nombreController.text;
+                      String apellidoCliente = _apellidoController.text;
+                      String companyCliente = _companyController.text;
+                      // Lógica para dar de alta el cliente
+                      postCliente(nombreCliente, apellidoCliente, companyCliente);
+                      // Puedes llamar a una función o realizar cualquier otra acción aquí
+                      print('Cliente dado de alta: $nombreCliente');
+                      Navigator.pop(context);
+      
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SuccessfulScreen()),
+                      );
+                    }
+                  },
+                  child: const Text('Guardar'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/src/forms/alta_form_trabajadores.dart
+++ lib/src/forms/alta_form_trabajadores.dart
@@ -22,149 +22,138 @@ class _AltaTrabajadorPageState extends State<AltaTrabajadorPage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Agregar nuevo trabajador"),
+      ),
       body: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.only(top: 5, left: 15),
-            width: size.width > 600 ? size.width * 0.8 : 500,
-            child: const Text(
-              'Agregar nuevo Trabajador',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 45, left: 15),
-            width: size.width > 600 ? size.width * 0.8 : 500,
-            child: const Text(
-              'Complete cada uno de los campos para dar de alta un nuevo trabajador',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-          ),
           formview(context),
         ],
       ),
     );
   }
 
-  Container formview(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 110, left: 15, right: 20),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _nombreController,
-              decoration: const InputDecoration(
-                labelText: 'Nombre del trabajador',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa el nombre del Trabajador';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _apellidosController,
-              decoration: const InputDecoration(
-                labelText: 'Apellidos',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa los apellidos del cliente';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _edadController,
-              decoration: const InputDecoration(
-                labelText: 'Edad',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa la edad del empleado';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _posicionController,
-              decoration: const InputDecoration(
-                labelText: 'Posicion',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingresa la posicion del empleado';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _salarioController,
-              decoration: const InputDecoration(
-                labelText: 'Salario',
-                border: OutlineInputBorder(),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Por favor, ingrese el salario del empleado';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              width: double.infinity, // Ocupar todo el ancho disponible
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    String nombreWorker = _nombreController.text;
-                    String apellidoWorker = _apellidosController.text;
-                    String edadWorker = _edadController.text;
-                    String positionWorker = _posicionController.text;
-                    String salarioWorker = _salarioController.text;
-                    // Lógica para dar de alta el cliente
-                    postTrabajador(nombreWorker, apellidoWorker, edadWorker,
-                        positionWorker, salarioWorker);
-                    // Puedes llamar a una función o realizar cualquier otra acción aquí
-                    Navigator.pop(context);
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SuccessfulScreen()),
-                    );
+  Center formview(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 600),
+        padding: const EdgeInsets.only(top: 110, left: 15, right: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Ingresa todos los datos del nuevo Trabajador",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _nombreController,
+                decoration: const InputDecoration(
+                  labelText: 'Nombre del trabajador',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa el nombre del Trabajador';
                   }
+                  return null;
                 },
-                child: const Text('Guardar'),
               ),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _apellidosController,
+                decoration: const InputDecoration(
+                  labelText: 'Apellidos',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa los apellidos del cliente';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _edadController,
+                decoration: const InputDecoration(
+                  labelText: 'Edad',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa la edad del empleado';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _posicionController,
+                decoration: const InputDecoration(
+                  labelText: 'Posicion',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa la posicion del empleado';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _salarioController,
+                decoration: const InputDecoration(
+                  labelText: 'Salario',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingrese el salario del empleado';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                width: double.infinity, // Ocupar todo el ancho disponible
+                child: FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      String nombreWorker = _nombreController.text;
+                      String apellidoWorker = _apellidosController.text;
+                      String edadWorker = _edadController.text;
+                      String positionWorker = _posicionController.text;
+                      String salarioWorker = _salarioController.text;
+                      // Lógica para dar de alta el cliente
+                      postTrabajador(nombreWorker, apellidoWorker, edadWorker,
+                          positionWorker, salarioWorker);
+                      // Puedes llamar a una función o realizar cualquier otra acción aquí
+                      Navigator.pop(context);
+      
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SuccessfulScreen()),
+                      );
+                    }
+                  },
+                  child: const Text('Guardar'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -174,7 +163,6 @@ class _AltaTrabajadorPageState extends State<AltaTrabajadorPage> {
     if (_formKey.currentState!.validate()) {
       String nombreCliente = _nombreController.text;
       // Lógica para dar de alta el cliente
-      // Puedes llamar a una función o realizar cualquier otra acción aquí
       print('Trabajador dado de alta: $nombreCliente');
       Navigator.pop(context);
     }
