b2238ac2ae0e5460d7aad3f9cbaafe63bbd92420
00lar <eduardani84@gmail.com>
2024-02-25 21:59:28 -0600

    moving to desktop

--- lib/src/view/finances/financesView.dart
+++ lib/src/view/finances/financesView.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:datafire/src/widgets/appBar.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
@@ -23,7 +24,7 @@ class _FinancesViewState extends State<FinancesView> {
     final response = await http.get(Uri.parse('http://localhost:3000/Api/v1/proyectos/egresos'));
 
     if (response.statusCode == 200) {
-      print("cargado con exito");
+      print("cargado con éxito");
       return List<Map<String, dynamic>>.from(json.decode(response.body));
     } else {
       throw Exception('Error al cargar datos de egresos');
@@ -35,37 +36,65 @@ class _FinancesViewState extends State<FinancesView> {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: AppBar(
-          title: Text("Finanzas"),
-        ),
+        child: AppBarDatafire(title: "Finanzas", description: "Aquí tendrás acceso a todas las finanzas de los proyectos"),
       ),
-      body: FutureBuilder(
-        future: fetchDataFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator(); // o cualquier widget de carga
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            dataSource = OrderInfoDataSource(snapshot.data as List<Map<String, dynamic>>);
-            return SfDataGrid(
-              source: dataSource,
-              columns: [
-                GridColumn(columnName: 'startDate', label: Text('Inicio de semana')),
-                GridColumn(columnName: 'endDate', label: Text('Fin de semana')),
-                GridColumn(columnName: 'weeklyCost', label: Text('Costo Semanal')),
-                GridColumn(columnName: 'totalWeeklyCost', label: Text('Total Semanal')),
-              ],
-              allowSorting: true,
-              allowFiltering: true,
-            );
-          }
-        },
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: DefaultTabController(
+              length: 4,
+              child: Column(
+                children: [
+                  const TabBar(tabs: [
+                    Tab(text: "egresos"),
+                    Tab(text: "nomina",),
+                    Tab(text: "ingresos",),
+                    Tab(text: "flujo")
+                  ]),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        FutureBuilder(
+                          future: fetchDataFuture,
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else {
+                              dataSource = OrderInfoDataSource(snapshot.data as List<Map<String, dynamic>>);
+                              return SfDataGrid(
+                                source: dataSource,
+                                columns: [
+                                  GridColumn(columnName: 'startDate', label: Text('Inicio de semana'), width: 200),
+                                  GridColumn(columnName: 'endDate', label: Text('Fin de semana'), width: 200),
+                                  GridColumn(columnName: 'weeklyCost', label: Text('Costo Semanal'), width: 400),
+                                  GridColumn(columnName: 'totalWeeklyCost', label: Text('Total Semanal')),
+                                ],
+                                allowSorting: true,
+                                allowFiltering: true,
+                                columnWidthMode: ColumnWidthMode.none,
+                                // defaultColumnWidth: 150,
+                              );
+                            }
+                          },
+                        ),
+                        Placeholder(),
+                        Placeholder(),
+                        Placeholder(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
 
 class OrderInfoDataSource extends DataGridSource {
   OrderInfoDataSource(List<Map<String, dynamic>> data) {
@@ -73,39 +102,47 @@ class OrderInfoDataSource extends DataGridSource {
         .map<DataGridRow>((e) => DataGridRow(cells: [
               DataGridCell<String>(columnName: 'startDate', value: e['startDate'] ?? ''),
               DataGridCell<String>(columnName: 'endDate', value: e['endDate'] ?? ''),
-              DataGridCell<double>(
-                  columnName: 'weeklyCost', value: _toDoubleOrZero(e['weeklyCost'])),
-              DataGridCell<double>(
-                  columnName: 'totalWeeklyCost', value: _toDoubleOrZero(e['totalWeeklyCost'])),
+              DataGridCell<List<Widget>>(columnName: 'weeklyCost', value: _formatWeeklyCost(e['weeklyCost'])),
+              DataGridCell<double>(columnName: 'totalWeeklyCost', value: (e['totalWeeklyCost'] ?? 0.0).toDouble()),
             ]))
         .toList();
   }
 
   late List<DataGridRow> _dataGridRows;
 
-  double _toDoubleOrZero(dynamic value) {
-    if (value is int || value is double) {
-      return value.toDouble();
-    } else {
-      return 0.0;
-    }
+  List<Widget> _formatWeeklyCost(List<dynamic>? weeklyCost) {
+    if (weeklyCost == null) return [];
+
+    return weeklyCost.map((entry) {
+      String projectName = entry['projectName'] ?? '';
+      double cost = entry['weeklyCost']?.toDouble() ?? 0.0;
+      return Text('$projectName: $cost');
+    }).toList();
   }
 
   @override
   List<DataGridRow> get rows => _dataGridRows;
 
-  // Resto del código...
-
-
   @override
   DataGridRowAdapter buildRow(DataGridRow row) {
     return DataGridRowAdapter(
       cells: row.getCells().map<Widget>((e) {
-        return Container(
-          alignment: getAlignment(e.columnName),
-          padding: EdgeInsets.all(8.0),
-          child: Text(e.value.toString()),
-        );
+        if (e.columnName == 'weeklyCost') {
+          List<Widget> weeklyCostWidgets = e.value as List<Widget>;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: weeklyCostWidgets,
+          );
+        } else {
+          return Container(
+            alignment: getAlignment(e.columnName),
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              e.value.toString(),
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
       }).toList(),
     );
   }
