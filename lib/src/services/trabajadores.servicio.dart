647208c7a301722324661e9ac8d5e8148b9e0734
Hugolifes <rach.rc36@gmail.com>
2024-04-12 15:28:04 -0600

    url production

--- lib/src/services/trabajadores.servicio.dart
+++ lib/src/services/trabajadores.servicio.dart
@@ -23,7 +23,8 @@ Future<List<dynamic>> fetchTrabajadores() async {
 
 Future<void> updateTrabajador(int id, String nombre, String lastName, int edad,
     String position, int salary) async {
-  final url = "http://localhost:3000/api/v1/trabajadores/SalaryUpdate/$id";
+  final url =
+      "https://datafire-production.up.railway.app/api/v1/trabajadores/SalaryUpdate/$id";
   Map<String, String> headers = await getAuthHeaders();
   try {
     final res = await http.patch(
